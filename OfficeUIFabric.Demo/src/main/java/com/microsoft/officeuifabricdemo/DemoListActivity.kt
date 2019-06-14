9ca798d7acbb312d3ec7b56cff443efc038074a6
Emily Lynam <emlynam@microsoft.com>
2019-06-14 20:24:12 +0000

    Merged PR 281394: Use list item and list item divider in the demo list
    Related work items: #736061

--- OfficeUIFabric.Demo/src/main/java/com/microsoft/officeuifabricdemo/DemoListActivity.kt
+++ OfficeUIFabric.Demo/src/main/java/com/microsoft/officeuifabricdemo/DemoListActivity.kt
@@ -6,19 +6,16 @@
 package com.microsoft.officeuifabricdemo
 
 import android.content.Intent
-import android.graphics.drawable.InsetDrawable
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import com.microsoft.officeuifabric.listitem.ListItemDivider
+import com.microsoft.officeuifabric.listitem.ListItemView
 import kotlinx.android.synthetic.main.activity_demo_list.*
 import kotlinx.android.synthetic.main.demo_list.*
-import kotlinx.android.synthetic.main.demo_list_content.view.*
 
 /**
  * This activity presents a list of [Demo]s, which when touched,
@@ -39,19 +36,9 @@ class DemoListActivity : AppCompatActivity() {
         toolbar.subtitle = com.microsoft.officeuifabric.BuildConfig.VERSION_NAME
 
         demo_list.adapter = DemoListAdapter()
-        addDemoListDivider()
+        demo_list.addItemDecoration(ListItemDivider(this, DividerItemDecoration.VERTICAL))
 
         Initializer.init(application)
-    }
-
-    private fun addDemoListDivider() {
-        val drawable = ContextCompat.getDrawable(this, com.microsoft.officeuifabric.R.drawable.ms_row_divider)
-        val spacing = resources.getDimension(R.dimen.default_layout_margin).toInt()
-        val insetDrawable = InsetDrawable(drawable, spacing, 0, spacing, 0)
-
-        val dividerItemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        dividerItemDecoration.setDrawable(insetDrawable)
-        demo_list.addItemDecoration(dividerItemDecoration)
     }
 
     private class DemoListAdapter : RecyclerView.Adapter<DemoListAdapter.ViewHolder>() {
@@ -65,13 +52,12 @@ class DemoListActivity : AppCompatActivity() {
         override fun getItemCount(): Int = DEMOS.size
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.demo_list_content, parent, false)
-            return ViewHolder(view)
+            return ViewHolder(ListItemView(parent.context))
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val demo = DEMOS[position]
-            holder.textView.text = demo.title
+            holder.listItem.title = demo.title
             with(holder.itemView) {
                 tag = demo
                 setOnClickListener(onClickListener)
@@ -79,7 +65,7 @@ class DemoListActivity : AppCompatActivity() {
         }
 
         private class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val textView: TextView = view.id_text
+            val listItem: ListItemView = view as ListItemView
         }
     }
 }
