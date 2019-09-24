f5e3a68a523fe250b7a64357524ad84fdc49a1bb
William Do <willdo@microsoft.com>
2019-09-24 18:07:40 +0000

    Merged PR 322418: Fixed landscape issue with Android N and below and added persistent date.
    Fixed landscape issue with Android N and below
    Added persistent date
    
    **Before**|**After**
    -|-
    ![Screenshot_1569290560.png](https://onedrive.visualstudio.com/4dcbf0bc-c3cd-49c8-a7c3-ec1924691d9b/_apis/git/repositories/32fa6338-45ea-42a0-aca0-484938e1962a/pullRequests/322418/attachments/Screenshot_1569290560.png) | ![Screenshot_1569290519.png](https://onedrive.visualstudio.com/4dcbf0bc-c3cd-49c8-a7c3-ec1924691d9b/_apis/git/repositories/32fa6338-45ea-42a0-aca0-484938e1962a/pullRequests/322418/attachments/Screenshot_1569290519.png)
    
    Related work items: #777725

--- OfficeUIFabric.Demo/src/main/java/com/microsoft/officeuifabricdemo/demos/CalendarViewActivity.kt
+++ OfficeUIFabric.Demo/src/main/java/com/microsoft/officeuifabricdemo/demos/CalendarViewActivity.kt
@@ -11,18 +11,44 @@ import com.microsoft.officeuifabric.util.DateStringUtils
 import com.microsoft.officeuifabricdemo.DemoActivity
 import com.microsoft.officeuifabricdemo.R
 import kotlinx.android.synthetic.main.activity_calendar_view.*
+import org.threeten.bp.Duration
 import org.threeten.bp.ZonedDateTime
 
 class CalendarViewActivity : DemoActivity() {
+    companion object {
+        private const val DATE = "date"
+    }
+
     override val contentLayoutId: Int
         get() = R.layout.activity_calendar_view
 
+    override val contentNeedsScrollableContainer: Boolean
+        get() = false
+
+    private var savedDate: ZonedDateTime? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         calendar_view.onDateSelectedListener = object : OnDateSelectedListener {
             override fun onDateSelected(date: ZonedDateTime) {
-                example_date_title.text = DateStringUtils.formatDateWithWeekDay(this@CalendarViewActivity, date)
+                setExampleDate(date)
             }
         }
+
+        (savedInstanceState?.getSerializable(DATE) as? ZonedDateTime)?.let {
+            setExampleDate(it)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(DATE, savedDate)
+    }
+
+    private fun setExampleDate(date: ZonedDateTime) {
+        savedDate = date
+        example_date_title.text = DateStringUtils.formatDateWithWeekDay(this, date)
+        calendar_view.setSelectedDateRange(date.toLocalDate(), Duration.ZERO, false)
     }
 }
