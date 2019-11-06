db20d7a9f2b6e19e8cfbeae95bd832e65c9a915a
William Do <willdo@microsoft.com>
2019-11-06 01:33:54 +0000

    Merged PR 340842: NumberPicker Number Landscape Spacing
    Aligned landscape `NumberPicker` item spacing with Outlook. Also imports got automatically organized.
    
    **Before**
    
    ![Screenshot_1572907779.png](https://onedrive.visualstudio.com/4dcbf0bc-c3cd-49c8-a7c3-ec1924691d9b/_apis/git/repositories/32fa6338-45ea-42a0-aca0-484938e1962a/pullRequests/340842/attachments/Screenshot_1572907779.png =600x)
    
    **After**
    
    ![Screenshot_1572907645.png](https://onedrive.visualstudio.com/4dcbf0bc-c3cd-49c8-a7c3-ec1924691d9b/_apis/git/repositories/32fa6338-45ea-42a0-aca0-484938e1962a/pullRequests/340842/attachments/Screenshot_1572907645.png =600x)
    
    Related work items: #791330

--- OfficeUIFabric/src/main/java/com/microsoft/officeuifabric/datetimepicker/DateTimePickerDialog.kt
+++ OfficeUIFabric/src/main/java/com/microsoft/officeuifabric/datetimepicker/DateTimePickerDialog.kt
@@ -13,16 +13,14 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import com.jakewharton.threetenabp.AndroidThreeTen
 import android.view.accessibility.AccessibilityEvent
-
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.microsoft.officeuifabric.R
 import com.microsoft.officeuifabric.calendar.OnDateSelectedListener
 import com.microsoft.officeuifabric.datetimepicker.DateTimePicker.PickerMode
