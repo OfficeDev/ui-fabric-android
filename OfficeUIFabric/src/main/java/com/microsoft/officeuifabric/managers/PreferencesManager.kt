6ca162a07ee33f0802bb9e55319bc7e1d4063523
Antonio Consuegra <anconsue@microsoft.com>
2020-03-16 10:59:53 +0000

    Merged PR 394498: Add setter method to write default start of the week in shared preferences
    ~~See different approach in !394497~~
    
    This is another approach to solve the same issue. In this case I am creating a setter to write a week start day in shared preferences. The downside of this approach is that we need changes in the calling app (see changed in the demo app).
    
    There could be a another approach, which is combine both of them. Please, feel free to suggest and choose your preferred solution.
    
    **Update:** I have combined both approaches in this PR, so it is now possible to write in shared preferences a different start of week, but in case there is no value written, the default from locale will be returned

--- OfficeUIFabric/src/main/java/com/microsoft/officeuifabric/managers/PreferencesManager.kt
+++ OfficeUIFabric/src/main/java/com/microsoft/officeuifabric/managers/PreferencesManager.kt
@@ -7,6 +7,8 @@ package com.microsoft.officeuifabric.managers
 
 import android.content.Context
 import org.threeten.bp.DayOfWeek
+import org.threeten.bp.temporal.WeekFields
+import java.util.Locale
 
 /**
  * [PreferencesManager] helper methods dealing with device SharedPreferences
@@ -16,12 +18,22 @@ object PreferencesManager {
     private const val PREF_KEY_WEEK_START = "weekStart"
 
     /**
+     * Sets the local first day of the week
+     */
+    @JvmStatic
+    fun setWeekStart(context: Context, weekStart: DayOfWeek) {
+        val sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        sharedPreferences.edit().putInt(PREF_KEY_WEEK_START, weekStart.value).apply()
+    }
+
+    /**
      * Gets the local first day of the week
      */
     @JvmStatic
     fun getWeekStart(context: Context): DayOfWeek {
+        val defaultWeekStart = WeekFields.of(Locale.getDefault()).firstDayOfWeek
         val sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
-        val weekStart = sharedPreferences.getInt(PREF_KEY_WEEK_START, DayOfWeek.SUNDAY.value)
+        val weekStart = sharedPreferences.getInt(PREF_KEY_WEEK_START, defaultWeekStart.value)
         return DayOfWeek.of(weekStart)
     }
 }
